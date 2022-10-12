ca4b879068d0e8391e009b308abbd34535b2fb44
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-10-12 23:47:21 +0900

    Initial Commit

---
+++ ARSample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARSample
+//
+//  Created by Ken Tsutsumi on 2022/10/12.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
